8ecdbbd6d397e92c4303354bc2f6c74074ee99d8
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2024-09-30 12:01:21 +0200

    fix: sophisticated produce error handling

--- chapter_four/goroutine_error_handling.go
+++ chapter_four/goroutine_error_handling.go
@@ -32,10 +32,17 @@ func ErrorHandling() {
 
 	done := make(chan interface{})
 	defer close(done)
-	urls := []string{"https://www.google.com", "https://badhost"}
+
+	errCount := 0
+	urls := []string{"a", "https://www.google.com", "https://badhost", "b", "c", "d"}
 	for result := range checkStatus(done, urls...) {
 		if result.Error != nil {
 			fmt.Printf("error: %v\n", result.Error)
+			errCount++
+			if errCount >= 3 {
+				fmt.Println("Too many errors, breaking!")
+				break
+			}
 			continue
 		}
 		fmt.Printf("Response: %v\n", result.Response.Status)
