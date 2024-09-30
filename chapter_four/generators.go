4536ba414d1fcb3e4b9a43de6b0d1683f72488be
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2024-09-30 22:08:46 +0200

    refactor: move generator functions to be visible inside the package

--- chapter_four/generators.go
+++ chapter_four/generators.go
@@ -5,75 +5,75 @@ import (
 	"math/rand/v2"
 )
 
+func repeat(done <-chan interface{}, values ...interface{}) <-chan interface{} {
+	valueStream := make(chan interface{})
+	go func() {
+		defer close(valueStream)
+
+		for _, v := range values {
+			select {
+			case <-done:
+				return
+			case valueStream <- v:
+			}
+		}
+	}()
+	return valueStream
+}
+
+func repeatFn(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+	valueStream := make(chan interface{})
+
+	go func() {
+		defer close(valueStream)
+
+		for {
+			select {
+			case <-done:
+				return
+			case valueStream <- fn():
+			}
+		}
+	}()
+	return valueStream
+}
+
+func toString(done <-chan interface{}, valueStream <-chan interface{}) <-chan string {
+	stringStream := make(chan string)
+
+	go func() {
+		defer close(stringStream)
+
+		for v := range valueStream {
+			select {
+			case <-done:
+				return
+			case stringStream <- v.(string):
+			}
+		}
+	}()
+
+	return stringStream
+}
+
+func take(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
+	takeStream := make(chan interface{})
+
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ {
+			select {
+			case <-done:
+				return
+			case takeStream <- <-valueStream:
+			}
+		}
+
+	}()
+	return takeStream
+}
+
 func Generators() {
-	repeat := func(done <-chan interface{}, values ...interface{}) <-chan interface{} {
-		valueStream := make(chan interface{})
-		go func() {
-			defer close(valueStream)
-
-			for _, v := range values {
-				select {
-				case <-done:
-					return
-				case valueStream <- v:
-				}
-			}
-		}()
-		return valueStream
-	}
-
-	repeatFn := func(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
-		valueStream := make(chan interface{})
-
-		go func() {
-			defer close(valueStream)
-
-			for {
-				select {
-				case <-done:
-					return
-				case valueStream <- fn():
-				}
-			}
-		}()
-		return valueStream
-	}
-
-	toString := func(done <-chan interface{}, valueStream <-chan interface{}) <-chan string {
-		stringStream := make(chan string)
-
-		go func() {
-			defer close(stringStream)
-
-			for v := range valueStream {
-				select {
-				case <-done:
-					return
-				case stringStream <- v.(string):
-				}
-			}
-		}()
-
-		return stringStream
-	}
-
-	take := func(done <-chan interface{}, valueStream <-chan interface{}, num int) <-chan interface{} {
-		takeStream := make(chan interface{})
-
-		go func() {
-			defer close(takeStream)
-			for i := 0; i < num; i++ {
-				select {
-				case <-done:
-					return
-				case takeStream <- <-valueStream:
-				}
-			}
-
-		}()
-		return takeStream
-	}
-
 	done := make(chan interface{})
 	defer close(done)
 
