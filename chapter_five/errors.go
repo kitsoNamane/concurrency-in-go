8a2d9df2eebd606c709ee42ffe21e6999fa58dbc
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2024-10-02 13:47:58 +0200

    feat: errors with proper wrapping

--- chapter_five/errors.go
+++ chapter_five/errors.go
@@ -50,9 +50,15 @@ func runJob(id string) error {
 	const jobBinPath = "/bad/job/binary"
 	isExecutable, err := isGloballyExec(jobBinPath)
 	if err != nil {
-		return err
+		return IntermediateErr{wrapError(err,
+			"cannot run job %q: requisite binaries not available",
+			id,
+		)}
 	} else if isExecutable == false {
-		return wrapError(nil, "job binary is not executable")
+		return wrapError(err,
+			"cannot run job %q: requisite binaries not available",
+			id,
+		)
 	}
 
 	return exec.Command(jobBinPath, "--id="+id).Run()
